27b61ae1ae5cb2a4ca26f6b28f1c43b33071b89a
swnordstrom <swnordstrom@email.wm.edu>
2019-10-27 12:15:58 -0600

    init coherent simulation file

---
+++ simulation_code.R
@@ -0,0 +1,23 @@
+### Structure of simulation
+
+### First, initialize parameters and population
+
+init.sim = function(params) {}
+# t = 1
+
+### Next, run through generations
+### For generation in generations 1:t
+
+## If there are individuals in the next generation,
+## Produce offspring
+
+# Intialize offspring data frame
+# Determine parents
+# Assign phenotypes
+# Age = 0, t = cur.gen
+
+
+## Next, determine adult survival
+
+# Age = 1, t = cur.gen
+
